e95d56e4abe2d5c8808c538a95f2cd60da5fd37a
Pawel Pabich <pawel@Pabich.eu>
2014-04-07 10:19:01 +1000

    Init
    Init

---
+++ TryingToTimoutAspNetMvcRequests/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TryingToTimoutAspNetMvcRequests.MvcApplication" Language="C#" %>
---
+++ TryingToTimoutAspNetMvcRequests/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TryingToTimoutAspNetMvcRequests.Startup))]
+namespace TryingToTimoutAspNetMvcRequests
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
